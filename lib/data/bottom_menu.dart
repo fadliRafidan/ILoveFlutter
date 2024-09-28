350f23a6d1c1c42188511a53522e1f509618f775
fadliRafidan <rafidfid28@gmail.com>
2024-09-28 18:55:53 +0700

    adjust icon

--- lib/data/bottom_menu.dart
+++ lib/data/bottom_menu.dart
@@ -9,5 +9,5 @@ List<BottomMenu> bottomMenu = [
   BottomMenu(0, 'assets/icons/home.png'),
   BottomMenu(1, 'assets/icons/heart.png'),
   BottomMenu(2, 'assets/icons/cart.png'),
-  BottomMenu(3, 'assets/icons/settings.png'),
+  BottomMenu(3, 'assets/icons/profil.png'),
 ];
